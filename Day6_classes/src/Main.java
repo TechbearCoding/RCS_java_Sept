6f2d582f5cc5efcd76b264fbc8624d75b2dc80b6
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2023-10-16 19:59:45 +0300

    classes sample

--- Day6_classes/src/Main.java
+++ Day6_classes/src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 3;
-        task1(a);
-        task1(6);
+//        int a = 3;
+//        task1(a);
+//        task1(6);
+
+//        Samples.sample1();
+
+//        int result = Samples.sample2();
+//        System.out.println(result);
+
+//        Samples.sample3("aa", "yy", "xx");
+
+        int result = Samples.sample4(5, 6);
+        System.out.println(result);
     }
 
-    static void task1(int number){
+    public static void task1(int number){
         if(number % 2 == 0){
             System.out.println("Pāra");
         }else{
